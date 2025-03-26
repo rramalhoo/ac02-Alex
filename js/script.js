88ad758d621fbd7ff4af744e112b03d3ba2857cf
rapharamalho <raphael.athenesi@gmail.com>
2025-03-26 17:44:08 -0300

    final commit

--- js/script.js
+++ js/script.js
@@ -1,41 +1,67 @@
-function checarVotacao() {
-    var idade = parseInt(document.getElementById("idade1").value);
-    var msg = document.getElementById("resultado1");
-    var brasileiroSim = document.getElementById("brasileiroCheck").checked;
-
-    if (brasileiroSim) {
-        if (idade >= 16) {
-            msg.textContent = "Você pode votar!";
-        } else {
-            msg.textContent = "Você não pode votar porque é menor de 16 anos.";
-        }
+function ex01() {
+    var idade = parseInt(document.getElementById("idade1").value)
+    var nacionalidade = document.getElementById("nacionalidade").value
+    var msg = ''
+  
+    if (idade >= 16 && nacionalidade === 'S') {
+      msg = 'Você pode votar !'
     } else {
-        msg.textContent = "Você não pode votar porque não é brasileiro(a).";
+      msg = 'Você não pode votar !'
     }
-}
+  
+    document.getElementById("resultado1").innerHTML = msg
+}  
 
 
 
 
 
+function ex02() {
+    var valor = parseFloat(document.getElementById("valor").value)
+    var origem = document.getElementById("origem").value
+    var destino = document.getElementById("destino").value
+    var resultado;
+  
+    if (origem === destino) {
+      resultado = valor
+    } else if (origem === "cm" && destino === "m") {
+      resultado = valor / 100
+    } else if (origem === "cm" && destino === "km") {
+      resultado = valor / 100000
+    } else if (origem === "m" && destino === "cm") {
+      resultado = valor * 100
+    } else if (origem === "m" && destino === "km") {
+      resultado = valor / 1000
+    } else if (origem === "km" && destino === "cm") {
+      resultado = valor * 100000
+    } else if (origem === "km" && destino === "m") {
+      resultado = valor * 1000
+    } else {
+      resultado = "Unidades invalidas"
+    }
+  
+    document.getElementById("resultado2").innerHTML = `${valor} ${origem} = ${resultado} ${destino}`
+  }
 
 
 
-function classificar() {
-    var idade = document.getElementById("idade3").value;
+
+
+function ex03() {
+    var idade = document.getElementById("idade3").value
     var msg = ''
     if (idade < 0) {
-        msg = 'Idade inválida.';
+        msg = 'Idade inválida.'
     } else if (idade <= 7) {
-        msg = 'Mirim';
+        msg = 'Mirim'
     } else if (idade <= 10) {
-        msg = 'Infantil';
+        msg = 'Infantil'
     } else if (idade <= 13) {
-        msg = 'Juvenil A';
+        msg = 'Juvenil A'
     } else if (idade <= 17) {
-        msg = 'Juvenil B';
+        msg = 'Juvenil B'
     } else {
-        msg = 'Adulto';
+        msg = 'Adulto'
     }
 
     document.getElementById("resultado3").innerHTML = msg
